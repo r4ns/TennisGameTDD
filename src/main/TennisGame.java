33c81e9775eeeaeca5fe5ebeab45ed45d34c3e3d
somename <someemail>
2017-05-18 20:38:27 +0200

    besouro automatic message

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -62,7 +62,7 @@ public class TennisGame
 		else if(player1Points==2 &&player2Points==3 ||player2Points==2 &&player1Points==3){
 			return score(player1Points)+" - " + score(player2Points);
 		}
-		else if(player1Points==4 && player2Points==4){
+		else if(player1Points==3 && player2Points==3){
 			return "deuce";
 		}
 		else
