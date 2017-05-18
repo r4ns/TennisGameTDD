73e722a2c8728f19fa0ec6174745c84778628830
somename <someemail>
2017-05-18 20:00:49 +0200

    besouro automatic message

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -9,6 +9,40 @@ public class TennisGame
 	public TennisGame() 
 	{
 		// TO BE IMPLEMENTED
+		this.player1Points=0;
+		this.player2Points=0;
 	}
+	
+	public void player1Scored(){
+		player1Points++;
+	}
+	
+	public void player2Score(){
+		player2Points++;
+	}
+	
+	public String score (int points){
+		if(points==0)
+			return "0";
+		else if(points==1)
+			return "15";
+		else if(points==2)
+			return "30";
+		else if(points==3)
+			return "40";
+		else
+		return "";
+	}
+	
+	public String score(){
+		if(player1Points==0 && player2Points==0){
+			return score(player1Points)+" - "+score(player2Points);	
+		}
+		else
+			return "";
+	}
+
+	
+	
 
 }
