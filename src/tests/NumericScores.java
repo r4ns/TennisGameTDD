92fbbdefb864bb5cb35ef4b5a53fea39b2e7bb99
somename <someemail>
2017-05-18 20:23:57 +0200

    besouro automatic message

--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -87,6 +87,16 @@ public class NumericScores {
 		
 		assertEquals("Player 1 and player two scored one time", "15 - 15", score);
 	}
+	@Test
+	public void test_1ScoredTwoAnd2ScoredOneTime(){
+		game=new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		String score=game.getScore();
+		
+		assertEquals("Player 1 and player two scored one time", "30 - 15", score);
+	}
 	
 	
 
