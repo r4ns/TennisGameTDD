787a7d8cdbce4aced1465ae682b22d12bfd7302c
somename <someemail>
2017-05-18 20:04:56 +0200

    besouro automatic message

--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -8,14 +8,25 @@ import main.TennisGame;
 
 // Testing of midgame scores before both players score three times
 public class NumericScores {
+	
+	TennisGame game;
 
 	
 	@Test
 	public void test_StartScore() {
-		TennisGame game = new TennisGame();
+		game = new TennisGame();
 		String score = game.getScore() ;
 
 		assertEquals("Initial score incorrect", "0 - 0", score);	
+	}
+	
+	@Test
+	public void testIfPlayer1ScoreOneTime(){
+		game=new TennisGame();
+		game.player1Scored();
+		String score=game.getScore();
+		
+		assertEquals("Player 1 scored one time", "15 - 0", score);
 	}
 	
 	
