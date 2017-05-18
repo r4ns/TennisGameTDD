af9e36b4e3b04f4f8f7cc2c9355e676f8e7c25d4
Robert Ramac <robert3ns@gmail.com>
2017-05-18 15:43:26 +0200

    Initial commit

---
+++ src/main/TennisGame.java
@@ -0,0 +1,14 @@
+package main;
+
+public class TennisGame 
+{	
+	private int player1Points;
+	private int player2Points;
+	private boolean gameEnded;
+	
+	public TennisGame() 
+	{
+		// TO BE IMPLEMENTED
+	}
+
+}
---
+++ src/tests/NumericScores.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// Testing of midgame scores before both players score three times
+public class NumericScores {
+
+	/*
+	@Test
+	public void test_StartScore() {
+		TennisGame game = new TennisGame();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "0 - 0", score);	
+	}
+	*/
+
+}
