e866e0cbd4b936e4f26cb4e0e0d970c46201aba5
IT2G2015 <IT2G2015@ws14.itc03.edu>
2017-05-18 20:57:58 +0200

    domaci 18.5.

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -8,7 +8,52 @@ public class TennisGame
 	
 	public TennisGame() 
 	{
-		// TO BE IMPLEMENTED
+		player1Points=0;
+		player2Points=0;
+		gameEnded=false;
 	}
+	
+	public String getScore()
+	{
+		if( player1Points>=3 && player1Points==player2Points){
+			return "deuce";
+		} else if(player1Points>3 && player2Points-player1Points==1){
+			return "Player2 advantage";
+		} else if(player1Points>3 && player1Points-player2Points==1){
+			return "Player1 advantage";
+		} else if(player1Points>3 && player1Points-player2Points>1){
+			return "Player1 wins";
+		} else if(player1Points>3 && player2Points-player1Points>1){
+				return "Player2 wins";
+		}else {
+			return score(player1Points)+ " - "+score(player2Points);
+		}
+	}
+	
+	public String score(int points)
+	{
+		if(points==0){
+			return "0";
+		}else if(points==1){
+			return "15";
+		}else if(points==2){
+			return "30";
+		}else if(points>=3){
+			return "40";
+		}
+		return "";
+		
+	}
+	public int player1Scored(){
+		
+		return player1Points++;
+	}
+	
+	public int player2Scored(){
+		
+		return player2Points++;
+	}
+	
+	
 
 }
