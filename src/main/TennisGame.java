f09cbab7ebc894f48b849ba2ca9eb23956a09082
Ozrensky <Ozrensky@EliteBook2560p>
2017-05-19 12:50:51 +0200

    TennisTDD v1.0

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -8,7 +8,40 @@ public class TennisGame
 	
 	public TennisGame() 
 	{
-		// TO BE IMPLEMENTED
+		player1Points = 0;
+		player2Points = 0;
+		gameEnded = false;
+	}
+	
+	public String getScore(){
+		String player1PointsString = "";
+		String player2PointsString = "";
+		if (!(player1Points >= 3) && !(player2Points >= 3)){
+			switch(player1Points){
+				case 0: player1PointsString = "0"; break;
+				case 1: player1PointsString = "1"; break;
+				case 2: player1PointsString = "2"; break;
+				case 3: player1PointsString = "3"; break;
+			}
+			switch(player2Points){
+				case 0: player2PointsString = "0"; break;
+				case 1: player2PointsString = "1"; break;
+				case 2: player2PointsString = "2"; break;
+				case 3: player2PointsString = "3"; break;
+			}
+			return player1PointsString + " - " +  player2PointsString;
+		} else if (player1Points == 3 && player2Points == 3){
+			return "deuce";
+		} else if (player1Points > player2Points){
+			return "player1 advantage";
+		} else {
+			return "player2 advantage";
+		}
+		
+	}
+	
+	public void player1Scores(){
+		
 	}
 
 }
--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -4,17 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.TennisGame;
+
 // Testing of midgame scores before both players score three times
 public class NumericScores {
-
-	/*
+	
 	@Test
 	public void test_StartScore() {
 		TennisGame game = new TennisGame();
 		String score = game.getScore() ;
-
 		assertEquals("Initial score incorrect", "0 - 0", score);	
 	}
-	*/
+	
+	@Test
+	public void test_Player1ScoresFirst(){
+		TennisGame game = new TennisGame();
+		game.player1Scores();
+		assertEquals("Player1 first scores incorrect", "15 - 0", game.getScore());
+	}
 
 }
