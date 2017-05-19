2df7dc7f909dc60bc6b62c2c9fdf5bd3cca0d3cd
IT35g2015 <IT35g2015@DESKTOP-6UCTQ20.itc03.edu>
2017-05-19 20:59:24 +0200

    IT35-2015

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -8,7 +8,107 @@ public class TennisGame
 	
 	public TennisGame() 
 	{
-		// TO BE IMPLEMENTED
+		
+	}
+	
+	public void player1Scored() throws TennisGameException
+	{
+		if(gameEnded)
+			throw new TennisGameException();
+		else
+		{
+			player1Points++;
+			checkGameEnded();
+		}
+	}
+	
+	public void player2Scored() throws TennisGameException
+	{
+		if(gameEnded)
+			throw new TennisGameException();
+		else
+		{
+			player2Points++;
+			checkGameEnded();
+		}
+	}
+	
+	private void checkGameEnded()
+	{ 
+		if((this.player1Points>=4 && this.player1Points-this.player2Points>=2) || (this.player2Points>=4 && this.player2Points-this.player1Points>=2)){
+			this.gameEnded=true;
+		}
+		else
+			this.gameEnded=false;
 	}
 
+	public String getScore()
+	{
+		if(this.player1Points==0 && this.player2Points==0){
+			return "0 - 0";
+		}
+		else if(this.player1Points==1 && this.player2Points==0){
+			return "15 - 0";
+		}
+		else if(this.player1Points==2 && this.player2Points==0){
+			return "30 - 0";
+		}
+		else if(this.player1Points==3 && this.player2Points==0){
+			return "40 - 0";
+		}
+		else if(this.player1Points==0 && this.player2Points==1){
+			return "0 - 15";
+		}
+		else if(this.player1Points==0 && this.player2Points==2){
+			return "0 - 30";
+		}
+		else if(this.player1Points==0 && this.player2Points==3){
+			return "0 - 40";
+		}
+		else if(this.player1Points==1 && this.player2Points==1){
+			return "15 - 15";
+		}
+		else if(this.player1Points==2 && this.player2Points==1){
+			return "30 - 15";
+		}
+		else if(this.player1Points==3 && this.player2Points==1){
+			return "40 - 15";
+		}
+		else if(this.player1Points==1 && this.player2Points==2){
+			return "15 - 30";
+		}
+		else if(this.player1Points==2 && this.player2Points==2){
+			return "30 - 30";
+		}
+		else if(this.player1Points==3 && this.player2Points==2){
+			return "40 - 30";
+		}
+		else if(this.player1Points==1 && this.player2Points==3){
+			return "15 - 40";
+		}
+		else if(this.player1Points==2 && this.player2Points==3){
+			return "30 - 40";
+		}
+		else if(this.player1Points>=3 && this.player2Points>=3 && this.player1Points==this.player2Points){
+			return "deuce";
+		}
+		else if(this.player1Points>=3 && this.player2Points>=3 && this.player1Points==this.player2Points+1){
+			return "advantage igrac1";
+		}
+		else if(this.player1Points>=3 && this.player2Points>=3 && this.player2Points==this.player1Points+1){
+				return "advantage igrac2";
+		}
+		else if(this.player1Points>this.player2Points){
+			checkGameEnded();
+			if(gameEnded)
+				return "pobedjuje igrac1";
+		}
+		else if(this.player2Points>this.player1Points){
+			checkGameEnded();
+			if(gameEnded)
+			return "pobedjuje igrac2";
+		}
+		
+		return "";
+	}
 }
