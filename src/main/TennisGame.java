51f0bf250ac5d669d9fcd14bd3230b526174f413
somename <someemail>
2017-05-19 11:59:29 +0200

    besouro automatic message

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -8,7 +8,25 @@ public class TennisGame
 	
 	public TennisGame() 
 	{
-		// TO BE IMPLEMENTED
+		player1Points=0;
+		player2Points=0;
+		gameEnded=false;
+	}
+	
+	public String getScore(int points){
+		if(points==0){
+			return "0";
+		}else if(points==1){
+			return "15";
+		}else if(points==2){
+			return "30";
+		}else if(points==3){
+			return "40";
+		}else
+			return "";
 	}
 
+	public String getScore(){
+		
+	}
 }
