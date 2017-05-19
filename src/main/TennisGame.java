9d3a10fa6f09e17d63e5bd2443bed4c76d168c61
it56g2015 <it56g2015@ws11.itc03.edu>
2017-05-19 13:06:43 +0200

    it56

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -8,7 +8,45 @@ public class TennisGame
 	
 	public TennisGame() 
 	{
-		// TO BE IMPLEMENTED
+		this.player1Points = 0;
+		this.player2Points = 0;
+		this.gameEnded = false;
 	}
 
+	public int getScore(int point)
+	{
+		if(point == 1)
+		{
+			return 15;
+		}
+		else if(point == 2)
+		{
+			return 30;
+		}
+		else if(point == 3)
+		{
+			return 40;
+		}
+		else{
+			return 0;
+		}
+	}
+	public String getScore()
+	{
+		this.player1Points = getScore(this.player1Points);
+		this.player2Points = getScore(this.player2Points);
+		if(this.player1Points == 40 && this.player2Points==40){
+			return "deuce";
+		}
+		else{
+			return this.player1Points + " - " + this.player2Points;
+		}
+	}
+	
+	public int player1Scored(){
+		return this.player1Points++;
+	}
+	public int player2Scored(){
+		return this.player2Points++;
+	}
 }
--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.TennisGame;
+
 // Testing of midgame scores before both players score three times
 public class NumericScores {
 
-	/*
+	
 	@Test
 	public void test_StartScore() {
 		TennisGame game = new TennisGame();
@@ -15,6 +17,167 @@ public class NumericScores {
 
 		assertEquals("Initial score incorrect", "0 - 0", score);	
 	}
-	*/
+	
+	@Test
+	public void test_p1Wins1Point_Score15_0()
+	{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		String score = game.getScore() ;
 
+		assertEquals("Initial score incorrect", "15 - 0", score);	
+	}
+	@Test
+	public void test_p1Wins2Point_Score30_0()
+	{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "30 - 0", score);	
+	}
+	@Test
+	public void test_p1Wins3Point_Score40_0()
+	{
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "40 - 0", score);	
+	}
+	@Test
+	public void test_p2Wins1Point_Score0_15()
+	{
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "0 - 15", score);	
+	}
+	@Test
+	public void test_p2Wins2Point_Score0_30()
+	{
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "0 - 30", score);	
+	}
+	@Test
+	public void test_p2Wins3Point_Score0_40()
+	{
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "0 - 40", score);	
+	}
+	@Test
+	public void test_p1Wins1Point_p2Wins1Point_Score15_15(){
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "15 - 15", score);
+	}
+	@Test
+	public void test_p1Wins2Point_p2Wins1Point_Score30_15(){
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player2Scored();
+		game.player1Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "30 - 15", score);
+	}
+	@Test
+	public void test_p1Wins3Point_p2Wins1Point_Score40_15(){
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player1Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "40 - 15", score);
+	}
+	@Test
+	public void test_p1Wins1Point_p2Wins2Point_Score15_30(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player2Scored();
+		game.player1Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "15 - 30", score);
+	}
+	@Test
+	public void test_p1Wins1Point_p2Wins3Point_Score15_40(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player2Scored();
+		game.player1Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "15 - 40", score);
+	}
+	@Test
+	public void test_p1Wins2Point_p2Wins3Point_Score30_40(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "30 - 40", score);
+	}
+	@Test
+	public void test_p1Wins3Point_p2Wins2Point_Score40_30(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "40 - 30", score);
+	}
+	@Test
+	public void test_p1Wins3Point_p2Wins3Point_Score40_40(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "deuce", score);
+	}
+	@Test
+	public void test_p1Wins4Point_p2Wins3Point_Score(){
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		String score = game.getScore() ;
+
+		assertEquals("Initial score incorrect", "player1 advantage", score);
+	}
 }
