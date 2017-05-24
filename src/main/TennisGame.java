e29528c374459aa4774922e40cc3465bf7d6d056
Milos Covilo <miloscovilo@hotmail.com>
2017-05-24 16:29:12 +0200

    Job done

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -5,10 +5,172 @@ public class TennisGame
 	private int player1Points;
 	private int player2Points;
 	private boolean gameEnded;
-	
+	public int pobeda = 0;
 	public TennisGame() 
 	{
 		// TO BE IMPLEMENTED
+		player1Points = 0;
+		player2Points = 0;
+		gameEnded = false;
+	}
+	
+	public void player1Scored() throws TennisGameException 
+	{
+		// TO BE IMPLEMENTED
+		/*checkGameEnded();
+		if(gameEnded)
+			throw new TennisGameException();
+		else*/
+		if(gameEnded == false)
+			this.player1Points++;
+		else
+			throw new TennisGameException("Player 1 pobedio");
+		
+	}
+
+	public void player2Scored() throws TennisGameException 
+	{
+		// TO BE IMPLEMENTED
+		/*checkGameEnded();
+		if(gameEnded)
+			throw new TennisGameException();
+		else*/
+		if(gameEnded == false)
+			this.player2Points++;
+		else
+			throw new TennisGameException("Player 2 pobedio");
+		
+	}
+	
+	private void checkGameEnded()
+	{ 
+		// TO BE IMPLEMENTED
+		if((player1Points > 3 && player1Points-2 >= player2Points))
+		{
+			
+			gameEnded = true;
+			pobeda = 1;
+			
+		} else if((player2Points > 3 && player2Points-2 >= player1Points))
+		{
+			gameEnded = true;
+			pobeda = 2;
+			
+		} else{
+			gameEnded = false;
+		}
+	}
+
+	public String getScore(int points) 
+	{
+		// TO BE IMPLEMENTED
+		
+		if(points == 0){
+			return "0";
+		}else if(points == 1){
+			return "15";
+		}else if(points == 2){
+			return "30";
+		}else if(points == 3){
+			return "40";
+		}else
+			
+		return "";
+	}
+	
+	public String getScore() 
+	{
+		checkGameEnded();
+		// Scores format: "player1Score - player2Score"
+		
+		
+		// "0 - 0"
+		if(player1Points == 0 && player2Points == 0)
+			return "0 - 0";
+		// "15 - 15"
+		else if(player1Points == 1 && player2Points == 1 )
+			return "15 - 15";
+		// "30 - 30"
+		else if(player1Points == 2 && player2Points == 2 )
+			return "30 - 30";
+		// "deuce"
+		else if(player1Points == 3 && player2Points == 3 )
+			return "deuce";
+		// "15 - 0", "0 - 15"
+		else if(player1Points == 1 && player2Points == 0)
+			return "15 - 0";
+		else if(player1Points == 0 && player2Points == 1)
+			return "0 - 15";
+		// "30 - 0", "0 - 30"
+		else if(player1Points == 2 && player2Points == 0)
+			return "30 - 0";
+		else if(player1Points == 0 && player2Points == 2)
+			return "0 - 30";
+		// "40 - 0", "0 - 40"
+		else if(player1Points == 3 && player2Points == 0)
+			return "40 - 0";
+		else if(player1Points == 0 && player2Points == 3)
+			return "0 - 40";
+		// "30 - 15", "15 - 30"
+		else if(player1Points == 2 && player2Points == 1)
+			return "30 - 15";
+		else if(player1Points == 1 && player2Points == 2)
+			return "15 - 30";
+		// "40 - 15", "15 - 40"
+		else if(player1Points == 3 && player2Points == 1)
+			return "40 - 15";
+		else if(player1Points == 1 && player2Points == 3)
+			return "15 - 40";
+		// "advantage player1"
+		else if(player1Points > 3 && player2Points == 3)
+			return "advantage player1";
+		// "advantage player2"
+		else if(player2Points > 3 && player1Points == 3)
+			return "advantage player2";
+		// "game player1"
+		else if(gameEnded == true && pobeda == 1)
+			return "game player1";
+		// "game player2"
+		else if(gameEnded == true && pobeda == 2)
+			return "game player2";
+		else if(player1Points == 3 && player2Points == 2)
+			return "40 - 30";
+		else if(player1Points == 2 && player2Points == 3)
+			return "30 - 40";
+		else if(player1Points > 3 && player2Points > 3 && player1Points == player2Points)
+			return "deuce";
+
+		// TO BE IMPLEMENTED
+		
+		
+		
+		
+		
+		/*if (gameEnded && pobeda == 1) 
+		{
+			return "game player1";
+			
+		} else if (gameEnded && pobeda == 2)
+		{
+			return "game player2";
+			
+		} else if (player1Points > 3 && player2Points > 3 && player1Points == player2Points)
+		{
+			return "deuce";
+			
+		} else if (player1Points >= 3 && player2Points >= 3 && player1Points-1 == player2Points)
+		{
+			return "advantage player1";
+			
+		} else if (player1Points >= 3 && player2Points >= 3 && player1Points == player2Points-1)
+		{
+			return "advantage player2";
+		} else
+		{
+			return getScore(player1Points) + " - " + getScore(player2Points);
+		}*/
+		return "";
+		
 	}
 
 }
--- src/tests/NumericScores.java
+++ src/tests/NumericScores.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.TennisGameException;
+import main.TennisGame;
+
 // Testing of midgame scores before both players score three times
 public class NumericScores {
 
@@ -16,5 +19,347 @@ public class NumericScores {
 		assertEquals("Initial score incorrect", "0 - 0", score);	
 	}
 	*/
+	
+	public NumericScores(){}
+
+	@Test
+	public void test_StartScore() {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Initial score incorrect", "0 - 0", score);	
+	}
+	
+	@Test
+	public void test_p1Wins1Point_Score15_0() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		//Act
+		game.player1Scored();
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored once", "15 - 0", score);	
+	}
+	
+
+	@Test
+	public void test_p1Wins2Points_Score30_0() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();		
+		game.player1Scored();
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored twice", "30 - 0", score);	
+	}
+	
+	@Test
+	public void test_p1Wins3Points_Score40_0() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times", "40 - 0", score);	
+	}
+	
+	@Test
+	public void test_p2Wins1Point_Score0_15() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player2 scored once", "0 - 15", score);	
+	}
+	
+	@Test
+	public void test_p2Wins2Points_Score0_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player2Scored();
+		game.player2Scored();
+		//Act		
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player2 scored twice", "0 - 30", score);	
+	}
+	
+	@Test
+	public void test_p2Wins3Points_Score0_40() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when playe2 scored three times", "0 - 40", score);	
+	}
+	
+	@Test
+	public void test_p1Wins1Point_p2Wins1Point_Score15_15() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when each player scored once", "15 - 15", score);	
+	}
+	
+	@Test
+	public void test_p1Wins2Points_p2Wins2Points_Score30_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();		
+		game.player2Scored();
+		game.player1Scored();		
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when each player scored two times", "30 - 30", score);	
+	}
+	
+	@Test
+	public void test_p1Wins2Points_p2Wins1Point_Score30_15() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored twice and player2 scored once", "30 - 15", score);	
+	}
+
+	@Test
+	public void test_p1Wins3Points_p2Wins1Point_Score40_15() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		game.player1Scored();
+		game.player1Scored();	
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored once", "40 - 15", score);	
+	}
+	
+	@Test
+	public void test_p1Wins1Point_p2Wins2Points_Score15_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored once and player2 scored twice", "15 - 30", score);	
+	}
+	
+	@Test
+	public void test_p1Wins1Point_p2Wins3Point_Score15_40() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();		
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored once and player2 scored three times", "15 - 40", score);	
+	}
+	
+	@Test
+	public void test_p1Wins2Points_p2Wins3Points_Score30_40() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored twice and player2 scored three times", "30 - 40", score);	
+	}
+	
+	@Test
+	public void test_p1Wins3Points_p2Wins2Points_Score40_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player2Scored();
+		
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice", "40 - 30", score);	
+	}
+	
+	@Test
+	public void test_p1Wins3Points_p2Wins2Points_AlternativeSequenceOfWins_Score40_30() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "40 - 30", score);	
+	}
+	
+	@Test
+	public void test_p1Wins3Points_p2Wins3Points_AlternativeSequenceOfWins_Score40_40() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "deuce", score);	
+	}
+	
+	@Test
+	public void test_p1Wins4Points_p2Wins4Points_AlternativeSequenceOfWins_Score_1() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "advantage player1", score);	
+	}
+	
+	@Test
+	public void test_p1Wins4Points_p2Wins4Points_AlternativeSequenceOfWins_Score_2() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player2Scored();
+		
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "advantage player2", score);	
+	}
+	
+	@Test
+	public void test_p1Wins5Points_p2Wins5Points_AlternativeSequenceOfWins_Score_2() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();
+
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player1Scored();
+		game.player2Scored();
+		
+		game.player2Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player1Scored();
+		
+		game.player2Scored();
+		game.player1Scored();
+		
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "deuce", score);	
+	}
+	
+	@Test
+	public void test_p1Wins() throws TennisGameException {
+
+		TennisGame game = new TennisGame();
+
+		if(game.pobeda == 1){
+			String score = game.getScore() ;
+			// Assert
+			assertEquals("Incorrect score when player1 scored three times and player2 scored twice. Players win points alternating.", "game player1", score);
+		}
+	}
+	
+	@Test
+	public void test_p2Wins() throws TennisGameException {
+
+		TennisGame game = new TennisGame();
+
+		if(game.pobeda == 2){
+			String score = game.getScore() ;
+			// Assert
+			assertEquals("Exeption.", "game player2", score);
+		}
+	}
+	
 
 }
