e7cbdd6cf1b360ef12c180a128cf65312dfd0beb
Aleksandar <Aleksandar@AsusT100>
2017-05-24 16:33:00 +0200

    Test

--- src/main/TennisGameException.java
+++ src/main/TennisGameException.java
@@ -2,5 +2,20 @@ package main;
 
 public class TennisGameException extends Exception 
 {
+	
+	String message;
+	public TennisGameException() {
+		super();
+	}
+	
+	TennisGameException(String message){
+		super(message);
+		//System.out.println(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
 
 }
+
