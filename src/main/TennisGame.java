fec3f4a5257714cd68ce52d3477c188b9bb2606e
ChristianKlecina <djfntt@yahoo.com>
2017-05-26 09:44:56 +0200

    Logika uradjena

--- src/main/TennisGame.java
+++ src/main/TennisGame.java
@@ -12,34 +12,40 @@ public class TennisGame
 	{
 		this.player1Points = player1Points = 0;
 		this.player2Points = player2Points = 0;
+		gameEnded = false;
 		
 	}
 	
 	public void player1Scored(){
-		if(player1Points == 0){
-			player1Points++;
-		} else if(player1Points == 1){
-			player1Points++;
-		} else if(player1Points == 2){
-			player1Points++;
-		} else if(player1Points == 3){
-			player1Points++;
-		} else
-			player1Points++;
+		if(gameEnded == false){
+			if(player2Points == 4){
+				player2Points --;
+			}else
+				player1Points ++;
+		}
+		checkGameEnded();
 		
 	}
 	
 	public void player2Scored(){
-		if(player2Points == 0){
-			player2Points++;
-		} else if(player2Points == 1){
-			player2Points++;
-		} else if(player2Points == 2){
-			player2Points++;
-		} else if(player2Points == 3){
-			player2Points++;
-		} else
-			player2Points++;
+		
+
+		if(gameEnded == false){
+			if(player1Points == 4){
+				player1Points --;
+			}else
+				player2Points ++;
+		}
+		checkGameEnded();
+	}
+	
+	private void checkGameEnded()
+	{ 
+		if((player1Points>=4 && player1Points - player2Points >= 2) || (player2Points >=4 && player2Points - player1Points >= 2)){
+			gameEnded = true;
+		}else {
+			gameEnded = false;
+		}
 	}
 	
 	
@@ -56,6 +62,8 @@ public class TennisGame
 		
 		case 4: return "advantage";
 		
+		case 5: return "win";
+		
 		default: return "";
 		
 		}
@@ -70,8 +78,10 @@ public class TennisGame
 			return "advantage player 1";
 		}  else if(getScore(player2Points) == "advantage" && getScore(player1Points) == "40"){
 			return "advantage player 2";
-		} 
-			
+		} else if(gameEnded == true && (getScore(player1Points) == "win" || getScore(player1Points) == "advantage"))
+			return "game player1";
+		else if(gameEnded == true && (getScore(player2Points) == "win" || getScore(player2Points) == "advantage"))
+			return "game player2";
 		else 
 			return getScore(player1Points) + " - " + getScore(player2Points);
 		
